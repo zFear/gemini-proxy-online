679c1334aa9bb306001bf6b54e33aee8402fd3c9
zFear <2687769@gmail.com>
2025-06-16 16:50:07 +1000

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- БЛОК АУТЕНТИФИКАЦИИ GOOGLE ---
+// --- БЛОК АУТЕНТИФИКАЦИИ вавававпвпвпвапапкпGOOGLE ---
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
