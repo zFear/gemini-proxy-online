574d5a2ce0aff6bcba570fffa50c002057e41afb
zFear <2687769@gmail.com>
2025-07-05 20:43:46 +1000

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -13,7 +13,7 @@ const auth = new google.auth.GoogleAuth({
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   },
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  scopes: ['[https://www.googleapis.com/auth/spreadsheets](https://www.googleapis.com/auth/spreadsheets)'],
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
@@ -122,4 +122,5 @@ app.post('/generate', async (req, res) => {
 
 
 module.exports = app;
+
 
